d288ad2a80144a59702b2571eba9510f77c5eb04
Ivan Silva <ivan.elisandro@gmail.com>
2026-07-10 02:32:21 -0300

    feat (blazor): add forms sample project

---
+++ Blazor/FormsSample/Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsSample.Models;
+
+/// <summary>
+/// Defines the structure of a feedback that can be included by an user.
+/// </summary>
+public class Feedback
+{
+    [Required(ErrorMessage = "Name cannot remain empty.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email cannot remain empty.")]
+    [EmailAddress(ErrorMessage = "You must enter a valid email address.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Comment cannot remain empty.")]
+    [StringLength(500, MinimumLength = 5, ErrorMessage = "Comment can have between 5 and 500 characters.")]
+    public string Comment { get; set; }
+}
---
+++ Blazor/FormsSample/Services/FeedbacksService.cs
@@ -0,0 +1,36 @@
+﻿using FormsSample.Models;
+
+namespace FormsSample.Services;
+
+/// <summary>
+/// Provides access to feedbacks added to the service.
+/// This is mocked behaviour to view the UI in action only.
+/// Usually this service would retrieve values from an API or database.
+/// </summary>
+public class FeedbacksService
+{
+    /// <summary>
+    /// Stores feedbacks to be presented.
+    /// </summary>
+    public List<Feedback> feedbacks = [];
+
+    /// <summary>
+    /// Gets all the feedbacks stored in memory.
+    /// </summary>
+    /// <returns>A collection of feedbacks.</returns>
+    public IEnumerable<Feedback> GetAll() => this.feedbacks;
+
+    /// <summary>
+    /// Adds a feedback to the collection in the memory.
+    /// </summary>
+    /// <param name="feedback">The feedback to add.</param>
+    public void Add(Feedback feedback)
+    {
+        if (feedback is null)
+        {
+            return;
+        }
+
+        this.feedbacks.Add(feedback);
+    }
+}
