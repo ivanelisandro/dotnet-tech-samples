3bbdba53fac266354d3a22b94f3936b3e90a2bfe
Ivan Silva <ivan.elisandro@gmail.com>
2026-07-20 02:01:34 -0300

    feat (blazor): add hybrid rendering app sample project

---
+++ Blazor/HybridRenderingSample/HybridRenderingSample/Services/MockDataService.cs
@@ -0,0 +1,28 @@
+﻿namespace HybridRenderingSample.Services;
+
+/// <summary>
+/// Service for mocking data retrieval.
+/// </summary>
+public class MockDataService
+{
+    /// <summary>
+    /// Stores mock sample data for testing purposes.
+    /// </summary>
+    private readonly List<string> _data =
+    [
+        "Test Data 1",
+        "Test Data 2",
+        "Test Data 3"
+    ];
+
+    /// <summary>
+    /// Retrieves the list of mocked data.
+    /// Simulates long running request.
+    /// </summary>
+    /// <returns>The list of mocked data.</returns>
+    public async Task<IReadOnlyCollection<string>> GetAll()
+    {
+        await Task.Delay(3000);
+        return _data;
+    }
+}
---
+++ Blazor/HybridRenderingSample/HybridRenderingSample/Services/TimeService.cs
@@ -0,0 +1,13 @@
+﻿namespace HybridRenderingSample.Services;
+
+/// <summary>
+/// Provides access to date and time information.
+/// </summary>
+public class TimeService
+{
+    /// <summary>
+    /// Retrieves the current date and time value for the server.
+    /// </summary>
+    /// <returns>Current date and time value.</returns>
+    public DateTime GetServerTime() => DateTime.Now;
+}
